7a4903335e67d00c07c266ae446a6d49c492271f
xh3b4sd <xh3b4sd@gmail.com>
2021-10-15 18:52:57 +0200

    raise price floor (#5)

--- pkg/step/st014/step.go
+++ pkg/step/st014/step.go
@@ -4,6 +4,7 @@ import (
 	"github.com/xh3b4sd/tracer"
 
 	"github.com/xh3b4sd/rsx/pkg/context"
+	"github.com/xh3b4sd/rsx/pkg/round"
 )
 
 type Step struct {
@@ -22,8 +23,57 @@ func (s Step) Ind() int {
 
 // ensure <Value> excess reserves in treasury
 func (s Step) Run(ctx context.Context) (context.Context, error) {
+	// Calculate the RSX circulating supply. Note that this subtracts the
+	// protocol debt as this is meant to be locked until accounted for by future
+	// revenue.
+	var rsa float64
+	{
+		rsa += ctx.Treasury.RSX.Amount
+
+		rsa -= ctx.Protocol.Debt.RSX.Amount
+	}
+
+	// Calculate the amount of DAI in the treasury used for backing RSX in
+	// circulation.
+	var daa float64
+	{
+		daa = ctx.Treasury.DAI.Amount
+	}
+
+	// Calculate the desired amount of DAI backing RSX in circulation.
+	var deb float64
+	{
+		deb = (rsa * ctx.RSX.Price.Floor)
+
+		deb = round.RoundP(deb, 0)
+	}
+
+	// Calculate the remaining excess reserves when accounted for with required
+	// RSX backing.
+	var exc float64
+	{
+		exc = ctx.Treasury.DAI.Amount - deb
+	}
+
+	// Account for the delta in backing against the excess reserves and the
+	// backing itself.
+	{
+		ctx.Treasury.ExcessReserves = exc
+	}
+
+	// Calculate the current amount of DAI backing RSX in circulation.
+	var cub float64
+	{
+		cub = daa - ctx.Treasury.ExcessReserves
+
+		cub = round.RoundP(cub, 0)
+	}
+
 	if ctx.Treasury.ExcessReserves != s.Value {
 		return context.Context{}, tracer.Maskf(executionFailedError, "expected %f, got %f", s.Value, ctx.Treasury.ExcessReserves)
+	}
+	if cub != deb {
+		return context.Context{}, tracer.Maskf(executionFailedError, "expected %f, got %f", deb, cub)
 	}
 
 	return ctx, nil
