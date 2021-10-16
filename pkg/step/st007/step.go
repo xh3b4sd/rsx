49f474d0524cbc3402a113d381f51f49b4a7e6de
Tim Schindler <xh3b4sd@gmail.com>
2021-10-16 17:25:00 +0200

    separate verifier and mutator for total supply

--- pkg/step/st007/step.go
+++ pkg/step/st007/step.go
@@ -1,9 +1,8 @@
-package st009
+package st007
 
 import (
-	"github.com/xh3b4sd/tracer"
-
 	"github.com/xh3b4sd/rsx/pkg/context"
+	"github.com/xh3b4sd/rsx/pkg/round"
 )
 
 type Step struct {
@@ -20,11 +19,16 @@ func (s Step) Ind() int {
 	return int(s.Index)
 }
 
-// verify: set <Value> RSX total supply
+// mutate: <amount> RSX total supply
 func (s Step) Run(ctx context.Context) (context.Context, error) {
-	if ctx.Treasury.RSX.Supply.Total != s.Value {
-		return context.Context{}, tracer.Maskf(executionFailedError, "expected %f, got %f", s.Value, ctx.Treasury.RSX.Supply.Total)
+	var amo float64
+	{
+		amo += ctx.Treasury.RSX.Minted
+
+		amo = round.RoundN(amo, 4)
 	}
+
+	ctx.Treasury.RSX.Supply.Total = amo
 
 	return ctx, nil
 }
