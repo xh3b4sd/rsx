9a57fe41eeee5d400bbd207f3cfa1001e6471b2c
xh3b4sd <xh3b4sd@gmail.com>
2021-10-19 16:51:12 +0200

    render graphs (#16)

--- pkg/chart/chart.go
+++ pkg/chart/chart.go
@@ -9,29 +9,45 @@ import (
 
 type Chart struct {
 	title string
-	xAxis []string
-	yAxis YAxis
+	xAxis XAxis
+	yAxis []YAxis
+}
+
+type XAxis struct {
+	values []string
 }
 
 type YAxis struct {
+	max    int
+	min    int
 	name   string
 	values []opts.LineData
 }
 
-func New(t string) *Chart {
+func New(t string, n ...string) *Chart {
 	c := &Chart{
 		title: t,
+	}
+
+	for _, v := range n {
+		c.yAxis = append(c.yAxis, YAxis{name: v})
+	}
+
+	if len(c.yAxis) == 0 {
+		c.yAxis = append(c.yAxis, YAxis{name: t})
 	}
 
 	return c
 }
 
 func (c *Chart) AddX(x int) {
-	c.xAxis = append(c.xAxis, fmt.Sprint(x))
+	c.xAxis.values = append(c.xAxis.values, fmt.Sprint(x))
 }
 
-func (c *Chart) AddY(y float64) {
-	c.yAxis.values = append(c.yAxis.values, opts.LineData{Value: y})
+func (c *Chart) AddY(y ...float64) {
+	for i, v := range y {
+		c.yAxis[i].values = append(c.yAxis[i].values, opts.LineData{Value: v})
+	}
 }
 
 func (c *Chart) Line() *charts.Line {
@@ -40,14 +56,28 @@ func (c *Chart) Line() *charts.Line {
 	l.SetGlobalOptions(
 		charts.WithTitleOpts(opts.Title{Title: c.title}),
 		charts.WithTooltipOpts(opts.Tooltip{AxisPointer: &opts.AxisPointer{Type: "cross"}, Show: true, Trigger: "axis"}),
+		charts.WithLegendOpts(opts.Legend{Show: true, Top: "3"}),
 		charts.WithXAxisOpts(opts.XAxis{Name: "Time"}),
-		charts.WithYAxisOpts(opts.YAxis{Name: "DAI"}),
+		charts.WithYAxisOpts(opts.YAxis{Max: c.yAxis[0].max, Min: c.yAxis[0].min, Name: "DAI"}),
 	)
 
-	l.SetXAxis(c.xAxis)
-	l.AddSeries(c.yAxis.name, c.yAxis.values)
+	l.SetXAxis(c.xAxis.values)
 
-	l.SetSeriesOptions(charts.WithLineChartOpts(opts.LineChart{Smooth: true}))
+	for i := range c.yAxis {
+		l.AddSeries(c.yAxis[i].name, c.yAxis[i].values)
+	}
 
 	return l
+}
+
+func (c *Chart) MaxY(m int) {
+	for i := range c.yAxis {
+		c.yAxis[i].max = m
+	}
+}
+
+func (c *Chart) MinY(m int) {
+	for i := range c.yAxis {
+		c.yAxis[i].min = m
+	}
 }
--- pkg/simulation/si002/si002.go
+++ pkg/simulation/si002/si002.go
@@ -1,6 +1,7 @@
 package si002
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -63,6 +64,8 @@ func Run() error {
 			return tracer.Mask(err)
 		}
 
+		p.SetLayout(components.PageFlexLayout)
+
 		err = p.Render(io.MultiWriter(f))
 		if err != nil {
 			return tracer.Mask(err)
@@ -83,16 +86,55 @@ func Run() error {
 }
 
 func generate(ctx context.Context) ([]*charts.Line, error) {
+	var floor float64
+	{
+		floor = 1.00
+	}
+
+	// multiple is the multiple to be applied to floor in order to compute the
+	// actual price floor. E.g. 2.00 means price ceiling is always 2x price
+	// floor.
+	var ceiling float64
+	var multiple float64
+	{
+		multiple = 2.00
+	}
+
+	// growth is the multiple at which price floor and price ceiling grow. E.g.
+	// 0.10 means the price floor rises by 10% on each increase.
+	var growth float64
+	{
+		growth = 0.02
+	}
+
 	var chs []*charts.Line
+
+	var backing *chart.Chart
+	{
+		backing = chart.New("DAI Backing / Market Cap", "DAI Backing", "Market Cap")
+		backing.MaxY(200e06)
+		backing.MinY(0)
+	}
 
 	var excess *chart.Chart
 	{
 		excess = chart.New("Excess Reserves")
+		excess.MaxY(15e06)
+		excess.MinY(0)
 	}
 
-	var backing *chart.Chart
+	var price *chart.Chart
 	{
-		backing = chart.New("DAI Backing")
+		price = chart.New("RSX Price", "Price Floor", "Price Ceiling")
+		price.MaxY(20)
+		price.MinY(0)
+	}
+
+	var supply *chart.Chart
+	{
+		supply = chart.New("RSX Supply", "Circulating Supply", "Total Supply")
+		supply.MaxY(20e06)
+		supply.MinY(0)
 	}
 
 	ctx = execute(ctx, []step.Interface{
@@ -109,24 +151,51 @@ func generate(ctx context.Context) ([]*charts.Line, error) {
 	})
 
 	for i := 0; i < 1000; i++ {
-		ctx = execute(ctx, []step.Interface{
-			st012.Step{Value: 1e05 /***/, Comment: "mutate: buy RSX for 100k DAI from pool"},
-			st015.Step{Value: 1e05 /***/, Comment: "mutate: arb RSX for 100k DAI between protocol and pool"},
-			st020.Step{ /***************/ Comment: "mutate: <amount> excess reserves in treasury"},
-		})
+		{
+			if backingCanIncrease(ctx, floor, growth) {
+				floor = floor * (1 + growth)
+				ceiling = floor * multiple
+
+				ctx = execute(ctx, []step.Interface{
+					st002.Step{Value: floor /*****/, Comment: fmt.Sprintf("mutate: set %.2f DAI price floor", floor)},
+					st003.Step{Value: ceiling /***/, Comment: fmt.Sprintf("mutate: set %.2f DAI price ceiling", ceiling)},
+					st020.Step{ /******************/ Comment: "mutate: <amount> excess reserves in treasury"},
+					st006.Step{ /******************/ Comment: "mutate: <amount> RSX circulating supply"},
+					st007.Step{ /******************/ Comment: "mutate: <amount> RSX total supply"},
+					st011.Step{ /******************/ Comment: "mutate: <amount> RSX market cap"},
+				})
+			}
+
+			ctx = execute(ctx, []step.Interface{
+				st012.Step{Value: 1e05 /***/, Comment: "mutate: buy RSX for 100k DAI from pool"},
+				st015.Step{Value: 1e05 /***/, Comment: "mutate: arb RSX for 100k DAI between protocol and pool"},
+				st020.Step{ /***************/ Comment: "mutate: <amount> excess reserves in treasury"},
+				st006.Step{ /***************/ Comment: "mutate: <amount> RSX circulating supply"},
+				st007.Step{ /***************/ Comment: "mutate: <amount> RSX total supply"},
+				st011.Step{ /***************/ Comment: "mutate: <amount> RSX market cap"},
+			})
+		}
 
 		{
 			backing.AddX(i)
-			backing.AddY(ctx.Treasury.DAI.Backing)
+			backing.AddY(ctx.Treasury.DAI.Backing, ctx.Pool.RSX.MarketCap)
 
 			excess.AddX(i)
 			excess.AddY(ctx.Treasury.DAI.Excess)
+
+			price.AddX(i)
+			price.AddY(ctx.RSX.Price.Floor, ctx.RSX.Price.Ceiling)
+
+			supply.AddX(i)
+			supply.AddY(ctx.Treasury.RSX.Supply.Circulating, ctx.Treasury.RSX.Supply.Total)
 		}
 	}
 
 	{
-		chs = append(chs, excess.Line())
 		chs = append(chs, backing.Line())
+		chs = append(chs, excess.Line())
+		chs = append(chs, price.Line())
+		chs = append(chs, supply.Line())
 	}
 
 	return chs, nil
@@ -143,4 +212,16 @@ func execute(ctx context.Context, stp []step.Interface) context.Context {
 	}
 
 	return ctx
+}
+
+func backingCanIncrease(ctx context.Context, floor float64, growth float64) bool {
+	// Calculate the amount of capital we can add to the price floor.
+	add := ctx.Treasury.DAI.Excess / ctx.Treasury.RSX.Supply.Circulating
+
+	// Calculate the amount of price floor increase we plan to do.
+	gro := floor * growth
+
+	// As long as the available capital to increase the price floor is smaller
+	// than the increase we want to achieve, we cannot increase the backing.
+	return add >= gro
 }
